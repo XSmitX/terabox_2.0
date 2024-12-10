607293430541b747a6a612797e090048638cabfe
Smit Panchal <91816167+XSmitX@users.noreply.github.com>
2024-12-10 23:03:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,10 +33,10 @@ def check_joined():
             if member_info.status in (ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER):
                 return True
             else:
-                await bot.send_message(chat_id, join_msg, reply_markup=ikm([[ikb("✅ Join Channel", url="https://t.me/teraupdate")]]))
+                await bot.send_message(chat_id, join_msg, reply_markup=ikm([[ikb("✅ Join Channel", url="https://t.me/xedbots")]]))
                 return False
         except Exception:
-            await bot.send_message(chat_id, join_msg, reply_markup=ikm([[ikb("✅ Join Channel", url="https://t.me/teraupdate")]]))
+            await bot.send_message(chat_id, join_msg, reply_markup=ikm([[ikb("✅ Join Channel", url="https://t.me/xedbots")]]))
             return False
 
     return filters.create(func)
